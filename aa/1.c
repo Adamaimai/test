4afb973263e3045c356c3f88eb182c57e4db485c
maimai <Ada.mai@fibocom.com>
2019-07-05 15:35:39 +0800

    add aa dir

---
+++ aa/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "wr.h"
+
+
+void Writeprint()
+{
+ printf("writer's imagination is always at work\n");
+  
+
+}
---
+++ aa/wr.h
@@ -0,0 +1,7 @@
+#ifndef WR_H_
+#define WR_H_
+
+void Writeprint();
+
+ 
+#endif
